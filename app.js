8d0578e8b548c2cf49c0e0eb3ad9492d8078ea8d
Ariba184 <142628941+Ariba184@users.noreply.github.com>
2023-10-05 15:39:29 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+console.log("practice excercise 1")
+    console.log(4 + 10);//printing that 4 + 10 is 14
+    console.log("My name is ariba");//
+    printing that my name is ariba
+    
+    
+    
+    console.log("practice exercise 2")
+    console.log("Hello world");//printing that hello world
+    
+    console.log("prictice exercise 3");//linked the file html
+    
+    console.log("practice exercise 4");
+    let a= 10;// i set a value of 10 
+    console.log("a");//printing that let a
+    /* This is a multiple-line comment.
+    it can be used to provide a brief outline of the purpose of the file.
+    In a real production script,you might include more detailed information*/
+
