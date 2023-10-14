7de90ad81a23613a1a88e0930acb4102bc004301
Ariba184 <142628941+Ariba184@users.noreply.github.com>
2023-10-14 22:48:38 +0500

    Add files via upload

---
+++ chapter2/miles.js
@@ -0,0 +1,4 @@
+let distance_miles =150;
+//1 mile is equals to 1.60934 kilometers
+let distance_km = distance_miles * 1.60934
+console.log(" the distance of "+ distance_miles + " miles is equals to" + distance_km + "kilometers");
